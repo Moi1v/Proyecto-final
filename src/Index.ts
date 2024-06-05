39910029189b64daf56f02d71fca3226fd73eb4e
Moi1v <cabreramoises102@gmail.com>
2024-06-04 21:04:28 -0600

    Git add agregado para usar

--- src/Index.ts
+++ src/Index.ts
@@ -4,3 +4,5 @@ import { LecturaUsuarios, crearUsuario, Eliminar } from "./interfaces/Mantenimie
 
 crearUsuario({id_usuario: 5 ,  nombre: 'Pablo Martinez', carnet: 2400089, correo: 'Pablom@hotmail.com', clave: 'P@bl0M@rt¡n3z' })
 // Eliminar(4)
+
+// git add .
