20d6e0dadfe4b135b9f849f6517189b98e88aeb7
Lourdes0310 <alvaradolourdes280@gmail.com>
2024-06-07 21:11:27 -0600

    Se agrego el interface de recetas

--- src/interfaces/receta.interface.ts
+++ src/interfaces/receta.interface.ts
@@ -1,7 +1,26 @@
 import { Medicamento } from './medicamento.interface'
 
+
 export interface Receta {
+    nombre: string
     id_paciente: number
     id_doctor: number
+    medicamentos?: Medicamento[]
+
+}
+
+export interface CrearRecetas{
+    nombre?: string
+    id_paciente: number
+    id_doctor: number
+    medicamentos?: Medicamento[]
+
+}
+
+export interface ActualizarRecetas{
+    nombre: string
+    id_paciente?: number
+    id_doctor: number
     medicamentos: Medicamento[]
+
 }
